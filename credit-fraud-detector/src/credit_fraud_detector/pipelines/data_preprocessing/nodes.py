fd5afc9c026b28cb9eafe3b349fb0cf86f503d89
MajdAlAjlanii <majdalajlani@gmail.com>
2024-05-26 17:53:21 +0100

    nodes in data_preprocessing

--- credit-fraud-detector/src/credit_fraud_detector/pipelines/data_preprocessing/nodes.py
+++ credit-fraud-detector/src/credit_fraud_detector/pipelines/data_preprocessing/nodes.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler, RobustScaler
 
 
@@ -22,4 +23,28 @@ def load_and_preprocess_data(filepath: str) -> pd.DataFrame:
     data.insert(0, 'scaled_amount', scaled_amount)
     data.insert(1, 'scaled_time', scaled_time)
 
-    return data
+    return preprocess_data(data)
+
+
+def shuffle_and_undersample(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.sample(frac=1)
+    fraud_df = df.loc[df['Class'] == 1]
+    non_fraud_df = df.loc[df['Class'] == 0][:492]
+    normal_distributed_df = pd.concat([fraud_df, non_fraud_df])
+    new_df = normal_distributed_df.sample(frac=1, random_state=42)
+    return new_df
+
+def remove_outliers(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    fraud_values = df[column].loc[df['Class'] == 1].values
+    q25, q75 = np.percentile(fraud_values, 25), np.percentile(fraud_values, 75)
+    iqr = q75 - q25
+    cut_off = iqr * 1.5
+    lower, upper = q25 - cut_off, q75 + cut_off
+    df = df.drop(df[(df[column] > upper) | (df[column] < lower)].index)
+    return df
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    df = shuffle_and_undersample(df)
+    for column in ['V14', 'V12', 'V10']:
+        df = remove_outliers(df, column)
+    return df
