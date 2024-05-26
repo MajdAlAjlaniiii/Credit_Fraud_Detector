9c4038efc4446003170a37263daa18ce3dbdac6e
MajdAlAjlanii <majdalajlani@gmail.com>
2024-05-26 15:34:51 +0100

    Data Preprocessing | Scaling and Distributing

--- credit-fraud-detector/src/credit_fraud_detector/pipelines/data_preprocessing/nodes.py
+++ credit-fraud-detector/src/credit_fraud_detector/pipelines/data_preprocessing/nodes.py
@@ -1,12 +1,25 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler, RobustScaler
+
 
 def load_and_preprocess_data(filepath: str) -> pd.DataFrame:
-    # Load the dataset
     data = pd.read_csv(filepath)
     
-    # Apply initial preprocessing steps
-    # Example: dropping rows with missing values
     data = data.dropna()
-    
-    # Additional preprocessing can be added here
+
+    # RobustScaler is less prone to outliers.
+    rob_scaler = RobustScaler()
+
+    data['scaled_amount'] = rob_scaler.fit_transform(data['Amount'].values.reshape(-1,1))
+    data['scaled_time'] = rob_scaler.fit_transform(data['Time'].values.reshape(-1,1))
+
+    data.drop(['Time','Amount'], axis=1, inplace=True)  
+
+    scaled_amount = data['scaled_amount']
+    scaled_time = data['scaled_time']
+
+    data.drop(['scaled_amount', 'scaled_time'], axis=1, inplace=True)
+    data.insert(0, 'scaled_amount', scaled_amount)
+    data.insert(1, 'scaled_time', scaled_time)
+
     return data
