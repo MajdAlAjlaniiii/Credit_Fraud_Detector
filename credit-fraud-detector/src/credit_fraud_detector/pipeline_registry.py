7e071ba728b2c94a8b743882f2c3b2f5a5850a4a
Vitor Neves <vitor.sneves@hotmail.com>
2024-06-22 22:21:41 +0100

    Implemented upload to FS. TODO: Implement feature download

--- credit-fraud-detector/src/credit_fraud_detector/pipeline_registry.py
+++ credit-fraud-detector/src/credit_fraud_detector/pipeline_registry.py
@@ -9,6 +9,7 @@ from credit_fraud_detector.pipelines import data_split
 from credit_fraud_detector.pipelines import dimensionality_reduction
 from credit_fraud_detector.pipelines import model_training
 from credit_fraud_detector.pipelines import model_evaluation
+from credit_fraud_detector.pipelines import upload_feature_store
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -18,15 +19,17 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
     data_preprocessing_pipeline = data_preprocessing.create_pipeline()
+    upload_feature_store_pipeline = upload_feature_store.create_pipeline()
     data_split_pipeline = data_split.create_pipeline()
     dimensionality_reduction_pipeline = dimensionality_reduction.create_pipeline()
     model_training_pipeline = model_training.create_pipeline()
     model_evaluation_pipeline = model_evaluation.create_pipeline()
     return {
         "data_preprocessing": data_preprocessing_pipeline,
+        'upload_feature_store': upload_feature_store_pipeline,
         "data_split": data_split_pipeline,
         "dimensionality_reduction": dimensionality_reduction_pipeline,
         "model_training": model_training_pipeline,
         "model_evaluation": model_evaluation_pipeline,
-        "__default__": data_preprocessing_pipeline + data_split_pipeline + dimensionality_reduction_pipeline + model_training_pipeline + model_evaluation_pipeline
+        "__default__": data_preprocessing_pipeline + data_split_pipeline + dimensionality_reduction_pipeline + model_training_pipeline + model_evaluation_pipeline + upload_feature_store_pipeline
     }
