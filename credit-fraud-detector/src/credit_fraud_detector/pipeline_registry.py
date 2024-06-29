d1f34faa1a7bf6717028844a5a31c675c47278e6
Maria Batrakova <73667985+whatswrongwithyourmitochondria@users.noreply.github.com>
2024-06-29 18:18:26 +0100

    Update pipeline_registry.py

--- credit-fraud-detector/src/credit_fraud_detector/pipeline_registry.py
+++ credit-fraud-detector/src/credit_fraud_detector/pipeline_registry.py
@@ -6,7 +6,6 @@ from kedro.pipeline import Pipeline
 
 from credit_fraud_detector.pipelines import data_preprocessing
 from credit_fraud_detector.pipelines import data_split
-from credit_fraud_detector.pipelines import dimensionality_reduction
 from credit_fraud_detector.pipelines import model_training
 from credit_fraud_detector.pipelines import model_evaluation
 from credit_fraud_detector.pipelines import upload_feature_store
@@ -30,7 +29,6 @@ def register_pipelines() -> Dict[str, Pipeline]:
     upload_feature_store_pipeline = upload_feature_store.create_pipeline()
     download_feature_store_pipeline = download_feature_store.create_pipeline()
     data_split_pipeline = data_split.create_pipeline()
-    dimensionality_reduction_pipeline = dimensionality_reduction.create_pipeline()
     model_training_pipeline = model_training.create_pipeline()
     model_evaluation_pipeline = model_evaluation.create_pipeline()
     data_drift_pipeline = data_drift.create_pipeline()
@@ -41,7 +39,6 @@ def register_pipelines() -> Dict[str, Pipeline]:
         "data_unit_tests": data_unit_tests_pipeline,
         'upload_feature_store': upload_feature_store_pipeline,
         "data_split": data_split_pipeline,
-        "dimensionality_reduction": dimensionality_reduction_pipeline,
         "model_training": model_training_pipeline,
         "model_evaluation": model_evaluation_pipeline,
         "download_feature_store": download_feature_store_pipeline,
@@ -50,7 +47,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         "model_selection": model_selection_pipeline,
         "__default__": data_preprocessing_pipeline + data_unit_tests_pipeline +\
             download_feature_store_pipeline + data_split_pipeline +\
-                dimensionality_reduction_pipeline + model_training_pipeline +\
-                    model_evaluation_pipeline +model_selection_pipeline+ upload_feature_store_pipeline +\
+                model_training_pipeline + model_evaluation_pipeline +\
+                     model_selection_pipeline+ upload_feature_store_pipeline +\
                         data_drift_pipeline + shap_pipeline
     }
